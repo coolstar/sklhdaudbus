9bc4fbf32c6c6d7d1870ce75cf4312bf9e169d0c
CoolStar <coolstarorganization@gmail.com>
2023-02-03 00:13:34 -0800

    Update driver.h

--- sklhdaudbus/driver.h
+++ sklhdaudbus/driver.h
@@ -32,6 +32,7 @@
 
 HDAUDIO_BUS_INTERFACE HDA_BusInterface(PVOID Context);
 HDAUDIO_BUS_INTERFACE_V2 HDA_BusInterfaceV2(PVOID Context);
+HDAUDIO_BUS_INTERFACE_V3 HDA_BusInterfaceV3(PVOID Context);
 
 #define IS_BXT(ven, dev) (ven == VEN_INTEL && dev == 0x5a98)
 
